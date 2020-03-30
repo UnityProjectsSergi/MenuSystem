66c1c71b3adc04edc2ebc118d4bb4f828cdffca0
SERGI-PORTATIL\Sergi Regi <sergiregi88@gmail.com>
2020-03-30 20:22:52 +0200

    Commit test brithnes

---
+++ NewMenuSaveLoad/Assets/Scripts/SaveSystem1/DataClasses/QualittySettingData.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace SaveSystem1.DataClasses
+{
+    [System.Serializable]
+    public class QualittySettingData : MonoBehaviour
+    {
+        
+        /// <summary>
+        /// Quality Settings.
+        /// </summary>
+        public GrapicsSettings currentQualityLevel = GrapicsSettings.Medium;
+        /// <summary>
+        /// Screen resoltion width.
+        /// </summary>
+        public int width = 1024;
+        /// <summary>
+        /// Screen resolution height.
+        /// </summary>
+        public int height = 768;
+        /// <summary>
+        /// True is game is in full screen.
+        /// </summary>
+        public bool isFullScreen;
+    }
+}
---
+++ NewMenuSaveLoad/Assets/brightness.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class brightness : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnGUI()
+    {
+       // RenderSettings.ambientLight = new Color(0f, 0f, 0f, 1);
+    //   RenderSettings.ambientMode = AmbientMode.Custom;
+    }
+
+}
